609e39d252ebc262eba32e2c217b90a06ca228f8
Szymon Cader <szymon.SC.cader@gmail.com>
2020-02-20 13:07:07 +0100

    Add travis support

--- jessdefiance/settings/local.py
+++ jessdefiance/settings/local.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 from .base import *
 
 DATABASES = {
@@ -7,5 +9,10 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER', 'jessdefiance'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'jessdefiance'),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        # 'PORT': os.environ.get('POSTGRES_PORT', 'localhost'),
     }
 }
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=False)
+DATABASES['default'].update(db_from_env)
+print(DATABASES)
