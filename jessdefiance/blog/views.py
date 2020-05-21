15c00a7dae8649af9807314936fc4bd12e643595
Szymon Cader <szymon.SC.cader@gmail.com>
2020-05-21 09:41:11 +0200

    Allow custom ordering of posts

--- jessdefiance/blog/views.py
+++ jessdefiance/blog/views.py
@@ -1,7 +1,7 @@
 from django.db.models import QuerySet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAdminUser
 
 from .filters import PostFilterSet, DynamicSearchFilter
@@ -25,16 +25,16 @@ class TagViewSet(viewsets.ModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     lookup_field = 'slug'
-    filter_backends = (DjangoFilterBackend, DynamicSearchFilter)
+    filter_backends = (DjangoFilterBackend, DynamicSearchFilter, OrderingFilter)
     search_fields = ('title',)
+    ordering_fields = ['title', 'publish_at', 'created_at']
     filterset_class = PostFilterSet
     anonymous_user_omit_fields = ['created_at', 'updated_at', 'published']
-
     def get_serializer(self, *args, **kwargs):
         omit_fields = self.anonymous_user_omit_fields if not self.request.user.is_staff else []
         return super().get_serializer(*args, **kwargs, omit=omit_fields)
 
     def get_queryset(self) -> 'QuerySet[Post]':
         if self.request.user.is_staff:
-            return Post.objects.all()
+            return Post.objects.order_by('-publish_at')
         return Post.objects.published().order_by('-publish_at')
