af992cf4bfb2e24ca856b5797e9e703562d10f78
Szymon Cader <szymon.SC.cader@gmail.com>
2020-06-04 17:01:35 +0200

    Add setting to recognize if request is https

--- jessdefiance/settings/production.py
+++ jessdefiance/settings/production.py
@@ -51,3 +51,5 @@ DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
